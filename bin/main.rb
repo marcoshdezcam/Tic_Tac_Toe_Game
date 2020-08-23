1e262281ac4dc605eff559078e0718584f801bbc
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-08-23 16:07:41 -0500

    Game: initialize, DRY play & create welcome. Board: ask  & mark.
    *Add next_move and moves to Player

--- bin/main.rb
+++ bin/main.rb
@@ -1,29 +1,35 @@
 #!/usr/bin/env ruby
 class Game
-  attr_reader :winning_coords
+  attr_accessor :tic_tac_board, :player1, :player2
 
   def initialize
-    @winning_coords = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    puts 'First player; please type your name!'
-    player1 = Player.new(gets.chomp, 'X')
-    puts 'Second player; please type your name!'
-    player2 = Player.new(gets.chomp, 'O')
-    puts "Welcome on board #{player1.name} and #{player2.name}, let the Tic-Tac-Toe game begins!"
-    puts %(Player 1: #{player1.name} has token: #{player1.token})
-    puts %(Player 2: #{player2.name} has token: #{player2.token})
+    @tic_tac_board = Board.new
+    puts %(First player; please type your name!)
+    @player1 = Player.new(gets.chomp, 'X')
+    puts %(Second player; please type your name!)
+    @player2 = Player.new(gets.chomp, 'O')
+  end
+
+  def welcome
+    puts %(Welcome on board #{@player1.name} and #{@player2.name}, let the Tic-Tac-Toe game begins!)
+    puts %(Player 1: #{@player1.name} has token: #{@player1.token})
+    puts %(Player 2: #{@player2.name} has token: #{@player2.token})
   end
 
   def play
-    tic_tac_board = Board.new
-    tic_tac_board.show_board
+    welcome
+    @tic_tac_board.mark_player_move(@player1, @tic_tac_board.ask_player_move(@player1))
+    @tic_tac_board.show_board
+    @tic_tac_board.mark_player_move(@player2, @tic_tac_board.ask_player_move(@player2))
+    @tic_tac_board.show_board
   end
 end
 
 class Board
+  attr_reader :winning_coords
   attr_accessor :game_board
 
   def initialize
-    @game_board = Array.new(3, Array.new(3))
     @game_board = [
       ['-', '-', '-'],
       ['-', '-', '-'],
@@ -32,13 +38,18 @@ class Board
   end
 
   def show_board
-    puts %(Show Game Board!)
     puts %(.................)
     @game_board.each { |itr| puts %( | #{itr[0]} | | #{itr[1]} | | #{itr[2]} |) }
+    puts %(.................)
   end
 
-  def mark_move(coords, token)
-    @game_board[coords] = token
+  def ask_player_move(player)
+    puts %(#{player.name}'s turn. Type a number from 1 to 9: )
+    player.next_move = gets.chomp.to_i
+  end
+
+  def mark_player_move(player, player_move)
+    puts %(#{player.name} move is #{player_move})
   end
 
   def check_winner
@@ -51,16 +62,16 @@ class Board
 end
 
 class Player
-  attr_accessor :name, :moves, :token
+  attr_accessor :name, :next_move, :moves, :token
 
   def initialize(name, token)
     @name = name
+    @next_move = []
+    @moves = [[]]
     @token = token
   end
 
   def move
-    puts %(Use numbers from 1 to 9.)
-    puts %(#{name} next move is: )
     move_coords = gets.chomp.to_i
     puts move_coords
   end
