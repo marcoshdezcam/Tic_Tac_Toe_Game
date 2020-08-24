707c8c4d6b156c3ccf4bd1cd70c83bc015b01ca4
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-08-24 12:17:17 -0500

    Correct merge errors. main.rb updated with new work

--- bin/main.rb
+++ bin/main.rb
@@ -66,7 +66,18 @@ class Board
   end
 
   def winner?
-    false
+    # Horizontal winning combinations
+    # [0][0]  [0][1]  [0][2] -> 1,2,3
+    # [1][0]  [1][1]  [1][2] -> 4,5,6
+    # [2][0]  [2][1]  [2][2] -> 7,8,9
+    # Vertical winning combinations
+    # [0][0]  [1][0]  [2][0] -> 1,4,7
+    # [0][1]  [1][1]  [2][1] -> 2,5,8
+    # [0][2]  [1][2]  [2][2] -> 3,6,9
+    # Diagonal winning combinations
+    # [0][0]  [1][1]  [2][2] -> 1,5,9
+    # [0][2]  [1][1]  [2][0] -> 3,5,7
+    
   end
 
   def marked?(player)
