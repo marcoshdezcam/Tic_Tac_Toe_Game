087e81f4e2ddec3d78354a3a577aff1f2ff993e0
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-08-26 18:36:57 -0500

    Improve Game.winner? by using Game.winning_moves

--- bin/main.rb
+++ bin/main.rb
@@ -16,7 +16,7 @@ loop do
   TicTacToe.mark_board(player1)
   TicTacToe.board_tic.show_board
   if TicTacToe.winner?(player1) || TicTacToe.draw?(player1)
-    puts %(Congratulations#{player1.name}, you won!) if TicTacToe.winner?(player1)
+    puts %(Congratulations #{player1.name}, you won!) if TicTacToe.winner?(player1)
     puts %(It's a draw!) if TicTacToe.draw?(player1)
     break
   end
--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,15 @@
 require_relative '../lib/board.rb'
-# rubocop: disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 class Game
-  attr_accessor :board_tic, :player1, :player2
+  attr_accessor :board_tic, :player1, :player2, :winning_moves
 
   def initialize(player1, player2)
     @board_tic = Board.new
     @player1 = player1
     @player2 = player2
+    @winning_moves = [
+      [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+      [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+    ]
   end
 
   def mark_board(player)
@@ -15,24 +18,14 @@ class Game
     @board_tic.game_board[0][player.next_move - 1] = player.token if player.next_move.between?(1, 3)
     @board_tic.game_board[1][player.next_move - 4] = player.token if player.next_move.between?(4, 6)
     @board_tic.game_board[2][player.next_move - 7] = player.token if player.next_move.between?(7, 9)
-    @board_tic.slots_taken << player.next_move
+    @board_tic.slots_taken << player.next_move && player.moves << player.next_move
   end
 
   def winner?(player)
-    if [@board_tic.game_board[0][0], @board_tic.game_board[0][1], @board_tic.game_board[0][2]].all?(player.token) ||
-       [@board_tic.game_board[1][0], @board_tic.game_board[1][1], @board_tic.game_board[1][2]].all?(player.token) ||
-       [@board_tic.game_board[2][0], @board_tic.game_board[2][1], @board_tic.game_board[2][2]].all?(player.token) ||
-       [@board_tic.game_board[0][0], @board_tic.game_board[1][0], @board_tic.game_board[2][0]].all?(player.token) ||
-       [@board_tic.game_board[0][1], @board_tic.game_board[1][1], @board_tic.game_board[2][1]].all?(player.token) ||
-       [@board_tic.game_board[0][2], @board_tic.game_board[1][2], @board_tic.game_board[2][2]].all?(player.token) ||
-       [@board_tic.game_board[0][0], @board_tic.game_board[1][1], @board_tic.game_board[2][2]].all?(player.token) ||
-       [@board_tic.game_board[0][2], @board_tic.game_board[1][1], @board_tic.game_board[2][0]].all?(player.token)
-      true
-    end
+    winning_moves.any?(player.moves)
   end
 
   def draw?(player)
     @board_tic.slots_taken.size == 9 && !winner?(player)
   end
 end
-# rubocop: enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
